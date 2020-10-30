1c59f4fbf0de352e8eff23ca8d78fe0dea5ffd78
aditya Kharade <adityakharade51@gmail.com>
2020-10-30 08:47:41 +0530

    Refactored the non query part as comments

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,9 +1,9 @@
-#Usecase1:
+--Usecase1:
 create database employee_payroll_service;
 show databases;
 use employee_payroll_service;
 
-#Usecase2:
+--Usecase2:
 create table employee_payroll_service(
     -> id               INT unsigned NOT NULL AUTO_INCREMENT,
     -> name             VARCHAR(150) NOT NULL,
@@ -13,21 +13,21 @@ create table employee_payroll_service(
     -> );
 describe employee_payroll_service;
 
-#Usecase3:
+--Usecase3:
 insert into employee_payroll_service(name, salary, start) VALUES
     -> ('BILL', 1000000.00, '2019-08-11'),
     -> ('Aniket', 2000000.00, '2019-11-08'),
     -> ('Sanket', 30000000.00, '2019-12-13');
 
-#Usecase4:
+--Usecase4:
 select * from employee_payroll_service;
 
-#Usecase5:
+--Usecase5:
 select salary from employee_payroll_service where name = 'Aniket';
 select salary from employee_payroll_service where start between cast('2019-12-13' as date) and date(NOW());
 select * from employee_payroll_service where start between cast('2019-08-11' as date) and date(NOW());
 
-#Usecase6:
+--Usecase6:
 alter table employee_payroll_service add gender char(1) after name;
 select * from employee_payroll_service;
 update employee_payroll_service  set gender = 'M' where name = 'Aniket';
@@ -35,7 +35,7 @@ update employee_payroll_service  set gender = 'M' where name = 'BILL' OR name = 
 select * from employee_payroll_service;
 describe employee_payroll_service;
 
-#Usecase7:
+--Usecase7:
 insert into employee_payroll_service(name, gender, salary, start) VALUES
     -> ('Deepika', 'F',2500000.00,'2018-12-11');
 select * from employee_payroll_service;
